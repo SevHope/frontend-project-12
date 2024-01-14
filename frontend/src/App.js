5b702bd86d105a0ea750e7780401631ac2a49ad7
Nadezhda Sevastianova <s.hope@bk.ru>
2024-01-14 12:18:31 +0300

    add Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { MainPage } from './pages/MainPage.jsx';
+import { LoginPage } from './pages/LoginPage.jsx';
+import { ErrorPage } from './pages/ErrorPage.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="error" element={<ErrorPage />} />
+        <Route path="login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,7 +1,14 @@
-export const MainPage = () => {
-    return (
-      <>
-        <div>Main</div>
-      </>
-    );
-  };
+import { Link } from 'react-router-dom';
+
+export const MainPage = () => (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/login">Login page</Link>
+        </li>
+        <li>
+          <Link to="/error">error page</Link>
+        </li>
+      </ul>
+    </nav>
+  );
